67e754bd103e0ae2a7162eeda54076cb7d6ce4ea
寺嶋朱里 <renkon0327@gmail.com>
2023-02-04 18:24:15 +0900

    色々変更しました!

--- akari.js
+++ akari.js
@@ -1,14 +1,28 @@
 $(function(){
 
+    // ハンバーガーメニュー
+
     $(".openbtn").click(function () {//ボタンがクリックされたら
         $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-        $("#g-nav").toggleClass('open');//ナビゲーションにpanelactiveクラスを付与
+        $("#g-nav").toggleClass('open');//ナビゲーションにopenクラスを付与
     });
     
     $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
         $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-        $("#g-nav").removeClass('open');//ナビゲーションのpanelactiveクラスも除去
+        $("#g-nav").removeClass('open');//ナビゲーションのopenクラスも除去
     });
+
+    // $('.your-class').slick({
+    //     	setting-name: setting-value
+    //     });
+
+    $(".slider").slick({
+        arrows: false,
+        dots: true,
+        autoplay: true
+    });
+
+
 
 
 
