42fd09f8500d458ac10e157261ae9c1a09532b74
米須千紗 <kihika.chiiii5@gmail.com>
2023-02-06 18:37:25 +0900

    合体！！

--- script.js
+++ script.js
@@ -23,8 +23,21 @@ $(function(){
         autoplay: true //自動再生する
     });
 
+     // 画像fadein
+     $(window).on("scroll",function () {
+        $(".fadein").each(function () {
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200) {
+                $(this).addClass("active");
+            };
+        });
+    });
 
-
-
+    // ページTOPへ戻るbtn
+    $(".top").on("click",function () {
+        $("body, html").animate({scrollTop: 0},500);
+    });
 
 })
