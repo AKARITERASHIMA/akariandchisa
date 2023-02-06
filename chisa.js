67676d6438b5e50d23c430df064235898c2238ae
米須千紗 <kihika.chiiii5@gmail.com>
2023-02-06 17:51:30 +0900

    画像のfadein追加

--- chisa.js
+++ chisa.js
@@ -1,4 +1,16 @@
 $(function () {
+    // 画像fadein
+    $(window).on("scroll",function () {
+        $(".fadein").each(function () {
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200) {
+                $(this).addClass("active");
+            };
+        });
+    });
+
     // ページTOPへ戻るbtn
     $(".top").on("click",function () {
         $("body, html").animate({scrollTop: 0},500);
