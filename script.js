652adba842f41144dada2cf1299313c00e18758d
寺嶋朱里 <renkon0327@gmail.com>
2023-02-06 18:32:53 +0900

    貼り付け

---
+++ script.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    // ハンバーガーメニュー
+
+    $(".openbtn").click(function () {//ボタンがクリックされたら
+        $(this).toggleClass("active");//ボタン自身に activeクラスを付与し
+        $("#g-nav").toggleClass('open');//ナビゲーションにopenクラスを付与
+    });
+    
+    $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn").removeClass("active");//ボタンの activeクラスを除去し
+        $("#g-nav").removeClass("open");//ナビゲーションのopenクラスも除去
+    });
+
+    // $('.your-class').slick({
+    //     	setting-name: setting-value
+    //     });
+
+    // スライダープラグイン『Slick』使用
+    $(".slider").slick({
+        arrows: false,
+        dots: true,
+        autoplay: true //自動再生する
+    });
+
+
+
+
+
+})
